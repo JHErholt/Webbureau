2da2f9044f69458aef433d75de8265bdd8bf2273
Jacob Erholt <56915193+JHErholt@users.noreply.github.com>
2021-12-10 12:53:12 +0100

    Lots of changes
    Header scss
    Footer scss
    copyrgiht
    +

--- src/js/script.js
+++ src/js/script.js
@@ -20,39 +20,18 @@ function toggleDropdown(current) {
     }
     clickedDropdown.classList.toggle('dropdown-active');
 }
-// Page Finder
-let path = window.location.pathname;
-let link = document.querySelector(`a[href='${path}']`);
-if (path === '/'){
-    console.log('Forside :)');
-}else{
-    link.style.color = 'rgb(178, 34, 34)';
-    link.style.textDecoration = 'underline';
-}
+
 
 
 
 
 // MAIL
+// function sendEmail ( _name, _email, _message) {
 
-
-function sendEmail ( _name, _email, _message) {
-
-}
-// var transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//       user: 'youremail@gmail.com',
-//       pass: 'yourpassword'
-//     }
-//   });
-
-//   console.log(nodemailer)
-
-
-function getValues(){
-    let _name = document.querySelector('#name');
-    let _email = document.querySelector('#email');
-    let _message = document.querySelector('#message');
-    sendEmail(_name, _email, _message);
-};
+// }
+// function getValues(){
+//     let _name = document.querySelector('#name');
+//     let _email = document.querySelector('#email');
+//     let _message = document.querySelector('#message');
+//     sendEmail(_name, _email, _message);
+// };
